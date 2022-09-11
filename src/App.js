e090b76da794ba281d48c1f50fcd228275999bc1
MJB <matt.berti13@gmail.com>
2022-09-11 14:32:35 -0500

    added logic for math functions and clear

--- src/App.js
+++ src/App.js
@@ -14,6 +14,13 @@ export const ACTIONS = {
 function reducer(state, {type, payload}){
   switch(type){
     case ACTIONS.ADD_DIGIT:
+      if(state.overwrite){
+        return{
+          ...state,
+          currentOperand: payload.digit,
+          overwrite: false,
+        }
+      }
       if(payload.digit === "0" && state.currentOperand === "0"){
       return state;
       }
@@ -24,7 +31,70 @@ function reducer(state, {type, payload}){
         ...state,
         currentOperand: `${state.currentOperand || ""}${payload.digit}`
       }
+      case ACTIONS.CHOOSE_OPERATION:
+        if(state.currentOperand == null && state.previousOperand == null){
+          return state
+        }
+        if(state.currentOperand == null){
+          return{
+            ...state,
+            operation: payload.operation,
+          }
+        }
+        if(state.previousOperand == null){
+          return {
+            ...state, 
+            operation: payload.operation,
+            previousOperand: state.currentOperand,
+            currentOperand: null
+          }
+        }
+
+        return{
+          ...state,
+          previousOperand: evaluate(state),
+          operation: payload.operation,
+          currentOperand: null
+        }
+    case ACTIONS.CLEAR: 
+    return {}
+    case ACTIONS.EVALUATE:
+      if(state.operation == null || state.currentOperand == null || state.previousOperand == null){
+        return state
+      }
+      return{
+        ...state,
+        overwrite: true,
+        previousOperand: null,
+        operation: null,
+        currentOperand: evaluate(state)
+      }
   }
+}
+
+function evaluate({ currentOperand, previousOperand, operation}){
+  const prev = parseFloat(previousOperand);
+  const current = parseFloat(currentOperand);
+  if(isNaN(prev) || isNaN(current)){
+    return ""
+  }
+  let computation = "";
+  switch(operation){
+    case "+":
+      computation = prev + current
+      break;
+    case "-":
+      computation = prev - current
+      break;
+    case "*":
+      computation = prev * current
+      break;
+    case "÷":
+      computation = prev / current
+      break;
+  }
+
+  return computation.toString();
 }
 
 function App() {
@@ -35,7 +105,7 @@ function App() {
         <div className="previous-operand">{previousOperand} {operation}</div>
         <div className="current-operand">{currentOperand}</div>
       </div>
-      <button className="span-two">AC</button>
+      <button className="span-two" onClick={() => dispatch({ type: ACTIONS.CLEAR})}>AC</button>
       <button>DEL</button>
       <OperationButton operation="÷" dispatch={dispatch}></OperationButton>
       <DigitButton digit="1" dispatch={dispatch}></DigitButton>
@@ -52,7 +122,7 @@ function App() {
       <OperationButton operation="-" dispatch={dispatch}></OperationButton>
       <DigitButton digit="." dispatch={dispatch}></DigitButton>
       <DigitButton digit="0" dispatch={dispatch}></DigitButton>
-      <button className="span-two">=</button>
+      <button className="span-two" onClick={() => dispatch({ type: ACTIONS.EVALUATE})}>=</button>
       
     </div>
   );
